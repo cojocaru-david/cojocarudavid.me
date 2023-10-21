173de478aa99d2825123427d590630d705d70d3e
David Cojocaru <david.marius.cojocaru@scoala4turnumagurele-tr.ro>
2023-10-21 20:44:25 +0300

    fixed more things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,22 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import { readFileSync } from "node:fs";
-import expressiveCode from "astro-expressive-code";
-import compress from "astro-compress";
+import mdx from '@astrojs/mdx';
 
-/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
-const astroExpressiveCodeOptions = {
-  styleOverrides: {
-    codeFontSize: '1rem',
-  }
-}
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), expressiveCode(astroExpressiveCodeOptions)],
+  integrations: [tailwind(), compressor(), mdx()],
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
+    formats: ["avif", "webp", "jpeg"]
   },
   site: "https://cojocarudavid.me",
-  buildOptions: {
-    sitemap: true
-  },
   vite: {
-    plugins: [rawFonts([".ttf", ".woff"]),compress()],
+    plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-      include: ["@astrojs/tailwind"]
+      exclude: ["@resvg/resvg-js"]
     }
   }
 });
