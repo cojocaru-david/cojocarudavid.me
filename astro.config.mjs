855588123c0622310e11677b5581b54058c45fb8
David Cojocaru <david.marius.cojocaru@scoala4turnumagurele-tr.ro>
2023-10-21 22:24:42 +0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { readFileSync } from "node:fs";
 import mdx from '@astrojs/mdx';
-import preact from "@astrojs/preact";
-
-
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor(), mdx(), preact()],
+  integrations: [tailwind(), compressor(), mdx()],
   image: {
     service: sharpImageService()
   },
