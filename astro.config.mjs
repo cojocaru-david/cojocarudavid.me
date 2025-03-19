b1314074841982970dc632d4efd494ffbd0a1123
cojocaru-david <contact@cojocarudavid.me>
2025-03-19 17:06:38 +0200

    chore(deps): upgrade Astro and TailwindCSS to latest versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,19 @@
+// @ts-check
 import { defineConfig, sharpImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import { readFileSync } from "node:fs";
-import mdx from '@astrojs/mdx';
-import compressor from "astro-compressor";
+
+import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
   site: "https://cojocarudavid.me",
-  vite: {
-    plugins: [rawFonts([".ttf", ".woff"])],
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+  integrations: [partytown(), icon()],
 });
-
-// vite plugin to import fonts
-function rawFonts(ext) {
-  return {
-    name: "vite-plugin-raw-fonts",
-    transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null
-        };
-      }
-    }
-  };
-}
